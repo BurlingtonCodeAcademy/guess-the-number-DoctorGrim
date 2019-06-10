41bb075a825c90c2611f9f295c2f3e9cd5924781
DoctorGrim <estlund.marshall@gmail.com>
2019-06-09 23:34:20 -0400

    added web app and made binary search cleaner

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function ask(questionText) {
 }
 
 start();
+parseInt;
 
 async function start() {
   console.log(
@@ -17,35 +18,42 @@ async function start() {
     "What is your secret number?\nI won't peek, I promise...\n"
   );
   console.log("You entered: " + secretNumber);
-  let min = await ask("what is the lowist number you might pick\n");
-  let max = await ask("and what is the highist\n");
+  let min = await ask("what is the lowest number you might pick\n");
+  let max = await ask("and what is the highest\n");
+  min = parseInt(min);
+  max = parseInt(max);
   let tries = 1;
   return guessTheNumber(min, max, tries);
-}
-let tries = 0;
+};
 async function guessTheNumber(min, max, tries) {
-  let mean = Math.floor((parseInt(min) + parseInt(max)+1) / 2); // use the mean rounded down as our guesses
+  let mean = Math.floor((min + max) / 2); // using the mean reduces the number of times we have to
   let response = await ask(
     "is this your number:... " +
       mean +
       "\nPress y for yes\nIf it is not press l if your number is lower and h if your number is higher\n"
   );
   if (response === "y") {
-    // check if guess is right, high or low repeat guess untile intager is found
+    // check if guess is right, high or low repeat guess until integer is found
     console.log(
-      "Your number was " + mean + "!\nI guessed it in " + tries + " tries."
+      "Your number was " +
+        mean +
+        "!\nI guessed it in " +
+        tries +
+        " tries.\nNow its my turn!\ntry and guess my number!"
     );
-    process.exit();
+    max = Math.floor(Math.random() * 1000);
+    humanTurn(Math.floor(Math.random() * Math.random() * max), max);
   } else if (response === "l") {
-    max = mean;
-    if (mean === (min + max) / 2) {  //check for cheating
+    max = mean - 1; //used to eliminate all impossible guesses, that which has been guessed and everything higher than it.
+    if (mean <= Math.ceil((min + max) / 2)) {
+      //check for cheating
       console.log("Cheater!");
       process.exit();
     }
-    guessTheNumber(min, max, tries + 1); //chainge min or max to mean and add to tries
+    guessTheNumber(min, max, tries + 1);
   } else if (response === "h") {
-    min = mean;
-    if (mean === (min + max) / 2) {
+    min = mean + 1;   //used to eliminate all impossible guesses, that which has been guessed and everything lower than it.
+    if (mean >= Math.floor((min + max) / 2)) {
       console.log("Cheater!");
       process.exit();
     }
@@ -53,5 +61,25 @@ async function guessTheNumber(min, max, tries) {
   } else {
     console.log("please enter y l or h");
     guessTheNumber(min, max, tries);
+  }
+}
+async function humanTurn(comNum, max) {
+  console.log("My number is between 1 and " + max);
+  let guess = await ask("type what you think my number is.\n");
+  guess = parseInt(guess);
+  if (guess === comNum) {
+    console.log(
+      "Wow you guessed my number it was:\n" + comNum + " lets switch again!"
+    );
+    start();
+  } else if (guess > comNum) {
+    console.log("Nope! My number is Lower. Try again.");
+    humanTurn(comNum, max);
+  } else if (guess < comNum) {
+    console.log("Nope! My number is Higher. Try again.");
+    humanTurn(comNum, max);
+  } else {
+    console.log("Please enter a number.");
+    humanTurn(comNum, max);
   }
 }
